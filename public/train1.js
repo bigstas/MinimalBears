5fd5d27378f5091cd569d250cc7373a1b0a26a01
bigstas <bigstas_lives@hotmail.com>
2015-12-17 23:00:17 +0000

    Words connected to database; altered styling and statefulness
    Styling changed - reduced / altered use of CSS file, more reliance on
    inline styles dependent on state.
    Buttons (actually divs) added for user to choose option (of two words).
    (Placeholder) feedback given on click.
    Commented out old code (should have updated this is a separate commit)
    flipped through pairs in a list to give button word values. Current
    code run as-is gets the words from the database.
    Pressing “progress” now flips between word pairs. This must be done
    before the buttons are pressed as I haven’t yet put in a mechanism to
    return to initial state.
    More use of state in the code. (TODO: move the state up to the highest
    possible level in the object hierarchy - simpler and corresponds to
    best practice.)
    Attempts to get sounds from the database and play them at the right
    time have so far failed. I seem to be able to get the objects from the
    database, but not to play them.

--- public/train1.js
+++ public/train1.js
@@ -1,4 +1,24 @@
-
+          
+var Nav = React.createClass({
+    render: function () {
+        return (
+            <div class="nav">
+                <div class="container">
+                    <ul>
+                        <li><a href="#">Logo</a></li>
+                        <li><a href="#">Train</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Sign Up</a></li>
+                        <li><a href="#">Log In</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+})
+          
 
 var ProgressBar = React.createClass({
     getInitialState: function () {
@@ -27,30 +47,41 @@ var Button = React.createClass({
 
 Parse.initialize('sQd6phAVnaN8vGtSWIHiLb0vcxr92gSL2EpyXNK8', '10tf0eOb5UcxDPWX7ECQ86HpATQYU7YJ9apnYXId');
 
-var OptionStyle = {
-    height: "40px",
-    width: "60px",
-    textAlign: "center",
-    backgroundColor: "aqua",
-    color: "white",
-    float: "{this.props.float}"
-}
 
-var LeftOption = React.createClass({
+var Option = React.createClass({
+    getInitialState: function () {
+        return {
+            clicked: false
+        };
+    },
+ 
+    handleClick: function (e) {
+        this.setState({
+            clicked: true
+        });
+        //alert("You are clicking me!");
+    },
+    
     render: function () {
+        if (this.props.feedback === "Correct!") {
+            var clickedStyle = { backgroundColor: "green" };
+        } else {
+            var clickedStyle = { backgroundColor: "red" };
+        }
+            
+        let unclickedStyle = {
+            backgroundColor: "aqua"
+        };
+        var text = this.state.clicked ? this.props.feedback : this.props.word;
+        
         return (
-            <div style={OptionStyle}>
-                Moo
-            </div>
-        );
-    }
-})
-
-var RightOption = React.createClass({
-    render: function () {
-        return (
-            <div style={OptionStyle}>
-                Quack
+            <div className='option'
+            onClick={this.handleClick}
+            style={(this.state.clicked) 
+                ? clickedStyle
+                : unclickedStyle
+            }>
+                {text}
             </div>
         );
     }
@@ -58,6 +89,13 @@ var RightOption = React.createClass({
 
 
 var Arena = React.createClass({
+    getInitialState: function () {
+        return {
+            counter: 0,
+            mode: "ask"
+        };
+    },
+    
     mixins: [ParseReact.Mixin], // Enable query subscriptions
 
     observe: function() {
@@ -65,7 +103,8 @@ var Arena = React.createClass({
     // The results will be available at this.data.comments
         return {
             word: (new Parse.Query('Language')).ascending('createdAt'),
-            sound: (new Parse.Query('Audio'))
+            item: (new Parse.Query('Item')).ascending('createdAt'),
+            sound: (new Parse.Query('Audio')).ascending('createdAt')
         };
     },
     
@@ -73,23 +112,51 @@ var Arena = React.createClass({
         document.getElementById("button").style.color = "red";
         //document.getElementById("button").value = this.data.word;
 
-        var snd = new Audio("Stapler.mp3"); // buffers automatically when created (what does this mean?)
-        snd.play();
+        // var snd = new Audio("Stapler.mp3"); // buffers automatically when created (what does this mean?)
+        // snd.play();
 
+        //this.data.sound.equalTo("filename", "stas thing")
+        //this.data.sound.find({
+        //var Audio = Parse.Object.extend("Audio");
+        var query = new Parse.Query("Audio");
+        query.equalTo("filename", "stas thing");
+        query.first({
+            success: function (object) {
+                alert("Successfully retrieved " + object);
+                var snd = new Audio(object.File);
+                snd.play();
+            },
+            error: function(error) {
+                alert("Error: " + error.code + " " + error.message);
+            } 
+        });
+        //var snd = new Audio(this.data.sound[0].File)
+        //snd.play()
         
-        //var barWidth = document.getElementById("progress").offsetWidth;
-        var fillWidthPercent = document.getElementById("fill").style.width.replace(/[^0-9]/g, '');
-        //if (fillWidth < barWidth -20) {
-        if (fillWidthPercent < 100 ) {
-            document.getElementById("fill").style.width = (fillWidthPercent + 10) + '%';
-            document.getElementById("button").value = toString(this.data.sound);
+        this.setState({
+            counter: this.state.counter +1
+        });
+        
+        var barWidth = document.getElementById("progress").offsetWidth;
+        var fillWidth = document.getElementById("fill").offsetWidth;
+        if (fillWidth < barWidth -20) {
+        // if (fillWidthPercent < 100 ) {
+            document.getElementById("fill").style.width = (fillWidth + 10) + 'px';
+            //document.getElementById("button").value = toString(this.data.sound);
         };
     },
     
     render: function () {
+        var currentWords = [
+            ["Moo", "Quack"],
+            ["Woof", "Meow"],
+            ["Wang", "Gav"]
+        ]
+        
         return (
             <div id="arena">
                 <p>This is the arena.</p>
+                <p>{this.data.sound[0]}</p>
                 <ProgressBar />
                 <ul>
                     {this.data.word.map(function(c) {
@@ -97,15 +164,32 @@ var Arena = React.createClass({
                     })}
                 </ul>
                 <Button handle={this.handleClick} />
-                <LeftOption float="left" />
-                <RightOption float="right" />
+                <div class="container">
+                    {/* <Option word={currentWords[this.state.counter][0]} feedback="Correct!" />
+                    <Option word={currentWords[this.state.counter][1]} feedback="Wrong!" />  */}
+                    {this.data.item.map(function(c) {
+                        return <Option word={c.Homophones[0]} feedback="Correct!" />
+                    })}
+                </div>
             </div>
         );
     }
 })
 
 
+var FullContent = React.createClass({
+    render: function () {
+        return (
+            <div id="outerDiv">
+                <Nav />
+                <Arena />
+            </div>
+        )
+    }
+})
+
+
 ReactDOM.render(
-    <Arena />,
+    <FullContent />,
     document.getElementById('content')
 );
