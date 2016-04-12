1f9af73faf5cf4972a1321c424ef5f6603b56325
bigstas <bigstas_lives@hotmail.com>
2016-04-12 12:18:33 +0100

    (minor correction)
    All the keys in INTERPRETED should be strings, just to make that clear.

--- client/static/translations.jsx
+++ client/static/translations.jsx
@@ -11,24 +11,24 @@ var INTERPRETED = {
         "polish": "Co to jest?"
     },
     train: {
-        english: "Train",
-        polish: "Ćwicz"
+        "english": "Train",
+        "polish": "Ćwicz"
     },
     profile: {
-        english: "Profile",
-        polish: "Profil"
+        "english": "Profile",
+        "polish": "Profil"
     },
     settings: {
-        english: "Settings",
-        polish: "Ustawienia"
+        "english": "Settings",
+        "polish": "Ustawienia"
     },
     welcome: {
         "english": "Welcome!",
         "polish": "Witaj!"
     },
     hometext: {
-        english: " is an app that will help you learn new sounds in foreign languages.",
-        polish: " to program który Ci pomoże w nauce nowych dźwięków w obcych językach."
+        "english": " is an app that will help you learn new sounds in foreign languages.",
+        "polish": " to program który Ci pomoże w nauce nowych dźwięków w obcych językach."
     }
 }
 
