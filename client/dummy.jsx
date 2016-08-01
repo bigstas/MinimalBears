50d331d16a8f256028ce48dd86e21077272231cf
guyemerson <g.e.t.emerson@googlemail.com>
2016-08-01 11:42:03 +0100

    Dummy class working with database access

--- client/dummy.jsx
+++ client/dummy.jsx
@@ -2,59 +2,43 @@ import React from 'react';
 import { connect } from 'react-apollo';
 import gql from 'graphql-tag';
 
-/*
-About = React.createClass({
+// A dummy class, that has one prop, "data".
+// The prop should be an object returned from PostGraphQL,
+// for a query for all rows in the "fakebear" table.
+// The class will display the "bar" field of the first row in the results.
+MyClass = React.createClass({
     render() {
-        return (
-            <div id='about'>
-                <h1>Minimal Bears: the research-based web app that improves your accent.</h1>
-                <h2>What is Minimal Bears?</h2>
-                <p>Minimal Bears is a web app that bla bla bla.</p>
-                <h2>How do you know that this stuff works?</h2>
-                <p>Your language id is {this.props.activeLanguageId} and your interface language is {this.props.interfaceLanguage}.</p>
-                <p>Research carried out by Mr Green shows that bla bla. The original research article is available <a href='#'>here</a>.</p>
-                <p>{this.props.data.name}</p>
-            </div>
-        );
+        // As the props come from an asynchronous query, we cannot assume everything is there
+        // If the data has not been returned yet:
+        var text = "placeholder"  
+        // If the data has been returned:
+        if (!this.props.data.loading) {
+            text = this.props.data.fakebearNodes.nodes[0].bar
+        }
+        // Render either way:
+        return <p>{text}</p>
     }
-});
-*/
+})
 
-function About({ data }) {
-    //return <p>{data.Language.name}</p>
-    return <p>Foo</p>
-}
-
-
+// react-apollo provides a "connect" function to wrap a class:
+// the wrapper fetches data, then gives it to the wrapped class as props.
+// "connect" requires a function to link queries to props:
+// this function should return an object where each property name is a prop name for the wrapped class,
+// and each property must itself have a property "query" which gives a GraphQL query.
+// (Properties other than "query" are for extra control on the functionality).
+// (The function can also take two arguments: the wrapper's props, and the redux state)
 function mapQueriesToProps({ ownProps, state }) {
     return {
-        data: gql`
-            {
-                Language(id: 1) {
-                    name
+        data: {
+            query: gql`{fakebearNodes {
+                nodes {
+                    foo
+                    bar
                 }
-            }
-        `
+            }}`
+        }
     }
 };
 
-export default connect({mapQueriesToProps})(About)
-//export default About
-
-/*
-        data: gql`
-            {
-                Language(id: 1) {
-                    name
-                }
-            }
-        `
-/*
-/*
-
-        fromQuery: gql`
-            {
-                query getFoo($myint Int!)
-            }
-        `
-*/
+// Wrap the class according to the function
+export default connect({mapQueriesToProps})(MyClass)
