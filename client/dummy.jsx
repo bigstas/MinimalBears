a953e6b55e898f49537671a6555288727ecad443
guyemerson <g.e.t.emerson@googlemail.com>
2017-01-28 20:08:19 +0000

    Avoid errors thrown by dummy code

--- client/dummy.jsx
+++ client/dummy.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { connect } from 'react-apollo'
+import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
 // A dummy class, that has one prop, "data".
 // The prop should be an object returned from PostGraphQL,
-// for a query for all rows in the "fakebear" table.
-// The class will display the "bar" field of the first row in the results.
+// for a query given at the bottom
+// The class will display the results.
 MyClass = React.createClass({
     render() {
         // As the props come from an asynchronous query, we cannot assume everything is there
@@ -13,7 +13,8 @@ MyClass = React.createClass({
         var text = "placeholder"  
         // If the data has been returned:
         if (!this.props.data.loading) {
-            text = this.props.data.fakebearNodes.nodes[0].bar
+            text = this.props.data.getCurrentUser.output
+            //text = this.props.data.authenticate
         }
         // Render either way:
         return <p>{text}</p>
@@ -27,19 +28,65 @@ MyClass = React.createClass({
 // and each property must itself have a property "query" which gives a GraphQL query.
 // (Properties other than "query" are for extra control on the functionality).
 // (The function can also take two arguments: the wrapper's props, and the redux state)
-function mapQueriesToProps({ ownProps, state }) {
-    return {
-        data: {
-            query: gql`{fakebearNodes {
-                nodes {
-                    foo
-                    bar
-                }
-            }}`
+
+let getUser = {
+    query: gql`mutation($input: GetCurrentUserInput!) {
+        getCurrentUser(input: $input) {
+            output
+        }
+    }`,
+    variables: {
+        input: {
+            clientMutationId: 'a'
         }
     }
 }
 
+let login = {
+    query: gql`query ($email: String!, $password: String!) {
+        authenticate(tryEmail: $email, tryPassword: $password)
+    }`,
+    variables: {
+        email: 'guido@example.com',
+        password: 'foobar'
+    }
+}
+
+let update = {
+    query: gql`mutation {
+        updateAudio(input: {rowId: 50000, newRowId: 60000}) {
+            audio {
+                rowId
+            }
+        }
+    }`
+}
+
+let submit = {
+    query: gql`mutation {
+        submitAudio (input: {
+            file: "1",
+            speaker: "Guy",
+            item: 15
+        }) {
+            output
+        }
+    }`
+}
+// see http://dev.apollodata.com/core/ ...
+// ... but remember we have the old version ...
+// they probably have some function that makes GraphQL mutations...
+// also, the instantiated ApolloClient object is passed to react-apollo's ApolloProvider...
+// is it a bad idea to instantiate a new ApolloClient?
+// http://dev.apollodata.com/react/mutations.html
+
+function mapQueriesToProps({ ownProps, state }) {
+    return {
+        data: getUser
+        //data: login
+    }
+}
+
 // Wrap the class according to the function
-export default connect({mapQueriesToProps})(MyClass)
+// export default connect({mapQueriesToProps})(MyClass)
 // hello from Gergo
