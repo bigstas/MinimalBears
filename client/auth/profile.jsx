4484cdafbb4e41dd4ebb2843e6e2289a3d2e4f8a
bigstas <bigstas_lives@hotmail.com>
2017-10-19 10:19:07 +0100

    react-chartjs-2 working
    Pie chart and mixed bar-line chart are displaying, both coloured and
    with legend. Pie chart is responsive to radio buttons.

--- client/auth/profile.jsx
+++ client/auth/profile.jsx
@@ -4,9 +4,9 @@
 import React from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
 import { Link } from 'react-router'
-import { Line, Bar, Radar, Pie } from 'react-chartjs' // Charts
+import { Line, Bar, Radar, Pie } from 'react-chartjs-2' // Charts
 // other charts available: Doughnut, ...
-import data from './chartdata'
+import chartdata from './chartdata'
 import { FacebookButton, FacebookCount, TwitterButton, TwitterCount } from "react-social"
     
 // Stand-in data, to be done in/from the database in whatever way is best and most efficient
@@ -44,21 +44,58 @@ User inputs:
         
     }
 }*/
-
-const ChartLegend = React.createClass({
-    render() {
-        const datasets = _.map(this.props.datasets, function (ds, index) { 
-            return <li key={index}><span className="legend-color-box" style={{ backgroundColor: ds.color }}></span> { ds.label }</li>
-        })
-
-        return (
-            <ul className={ this.props.title + "-legend" }>
-                { datasets }
-            </ul>
-        )
-    }
-})
    
+const mixOptions = {
+  responsive: true,
+  tooltips: {
+    mode: 'label'
+  },
+  elements: {
+    line: {
+      fill: false
+    }
+  },
+  scales: {
+    xAxes: [
+      {
+        display: true,
+        gridLines: {
+          display: false
+        },
+        labels: {
+          show: true
+        }
+      }
+    ],
+    yAxes: [
+      {
+        type: 'linear',
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+        gridLines: {
+          display: false
+        },
+        labels: {
+          show: true
+        }
+      },
+      {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        id: 'y-axis-2',
+        gridLines: {
+          display: false
+        },
+        labels: {
+          show: true
+        }
+      }
+    ]
+  }
+}
+
 
 // when there is a logged in user
 const UserProfile = React.createClass({
@@ -66,27 +103,16 @@ const UserProfile = React.createClass({
         return {language: '0'}
     },
     
-    componentDidMount: function () {
-        // prepare the legend
-        const legend = this.refs.chart.getChart().generateLegend()
-        this.setState({ legend: legend })
-    },
-
-    
     handleOptionChange(event) {
         const language = event.target.value
         this.setState({ language: language })
     },
     
     render() {
-        const legend = this.state && this.state.legend || ''
-        
-        console.log(data)    
-    
-        const pieChartData = data.pieChartData[this.state.language]
-        const lineChartData = data.lineChartData
-        const barChartData = data.barChartData
-        const radarChartData = data.radarChartData
+        // creating a copy so that the imported chartdata object is not written over
+        const newData = JSON.parse(JSON.stringify(chartdata))
+        const pieChartData = newData.pieChartData[this.state.language]
+        const mixChartData = newData.mixChartData
         
         // this object is required, but it can be empty...
         const chartOptions = {
@@ -117,15 +143,14 @@ const UserProfile = React.createClass({
                         <input type="radio" value="1" checked={this.state.language === '1'} onChange={this.handleOptionChange} />Polish
                     </label>
             {/* CHARTS */}
-                    <Pie data={pieChartData} options={chartOptions} ref='chart' />
-                    <ChartLegend datasets={data.pieChartData[0]} title="PieChart" />
-                    {/*<div dangerouslySetInnerHTML={{ __html: legend }} />*/}
-                    <h4>Your XP points over time</h4>
+                    <Pie data={pieChartData} />
+                    <Bar data={mixChartData} options={mixOptions} />
+                    {/*<h4>Your XP points over time</h4>
                     <Line data={lineChartData} options={chartOptions} />
                     <h4>Average success over time</h4>
                     <Bar data={barChartData} options={chartOptions} />
                     <h4>Relative ease of contrasts</h4>
-                    <Radar data={radarChartData} options={chartOptions} />
+                    <Radar data={radarChartData} options={chartOptions} />*/}
                 </div>
                 <TwitterButton className="button" id="twitterButton" element="div" url={url}>
                         {" Share us on Twitter"}
