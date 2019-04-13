fd3aba9ee1b3775f31ba3918daa8ebbf5061741f
sack-art-velo <iamsackartvelo@protonmail.com>
2019-04-13 10:30:44 +0100

    Set up skeletal GraphQL queries for moderation

--- client/audio/edit.jsx
+++ client/audio/edit.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Translate from 'react-translate-component'
 import Peaks from 'peaks.js'
+import { withRouter } from 'react-router' // TODO: is this necessary?
+import gql from 'graphql-tag'
+import { graphql, compose } from 'react-apollo'
+import LoadingPage from '../auxiliary/loading'
 
 function NoEditPage(props) {
     return (
@@ -16,31 +20,19 @@ class PeaksObject extends React.Component {
         console.log("this.props.src is " + this.props.src)
         return (
             <div>
-                <div id='audioContainer' ref={this.audioContainerRef.bind(this)} />
-                <audio id='mainAudio' ref={this.mainAudioRef.bind(this)} src={this.props.src} controls={false} />
+                <div id='audioContainer' ref="audioContainer" />
+                <audio id='mainAudio' ref="mainAudio" src={this.props.src} controls={false} />
             </div>
         )
     }
     
-    audioContainerRef(el) {
-    	this.audioContainer = el
-    	console.log('define audioContainer ref')
-    	console.log(el)
-    }
-    
-    mainAudioRef(el) {
-    	this.mainAudio = el
-    	console.log('define mainAudio ref')
-    	console.log(el)
-	}
-    
     updatePeaksObject() {
-        console.log(this.mainAudio.src)
-        console.log(this.mainAudio.duration)
+        console.log(this.refs.mainAudio.src)
+        console.log(this.refs.mainAudio.duration)
         
         this.instance = Peaks.init({
-            container: this.audioContainer,
-            mediaElement: this.mainAudio,
+            container: this.refs.audioContainer,
+            mediaElement: this.refs.mainAudio,
             audioContext: this.myAudioContext,
             keyboard: false,
             height: 200,
@@ -64,12 +56,12 @@ class PeaksObject extends React.Component {
     }
     
     componentDidMount() {
-    	this.mainAudio.addEventListener('loadeddata', this.updatePeaksObject.bind(this))
+        this.refs.mainAudio.addEventListener('loadeddata', this.updatePeaksObject.bind(this))
 	}
     
     shouldComponentUpdate(nextProps, nextState) {
-        this.mainAudio.src = nextProps.src
-        this.mainAudio.load()
+        this.refs.mainAudio.src = nextProps.src
+        this.refs.mainAudio.load()
         this.instance.destroy()
         
         return false
@@ -97,21 +89,53 @@ class EditingPage extends React.Component {
         if (segment) {
             const segment = this.refs.PeaksObject.instance.segments.getSegments()[0]
             const times = [segment.startTime, segment.endTime]
+            this.props.submitEdits({variables: {input: {
+                file: "", // TODO!
+                fromStart: {
+                    seconds: 0.1
+                },
+                fromEnd: {
+                    seconds: 0.1
+                },
+                volume: 0.9
+            }}}).then((response) => {
+                console.log("Success approving audio!")
+                console.dir(response)
+            }).catch((error) => {
+                console.log("error on edit")
+                console.error(error)
+            })
             console.log(times)
         } else {
-            // do something to accept the entire audio file as ok
+            this.handleApproval(true, () => {}) // any callback needed?
         }
         this.setState({ whichSrc: this.state.whichSrc +1 })
         alert("On to the next audio clip!")
     }
     
     handleReject() {
-        this.setState({ whichSrc: this.state.whichSrc +1 })
+        this.handleApproval(false,
+            () => this.setState.bind(this, { whichSrc: this.state.whichSrc +1 }) // do we need to bind(this)?
+        )
         // some sort of dialogue box concerning why the audio has been rejected?
+    }
+
+    handleApproval(approved, callback) {
+        this.props.approveAudio({variables: {input: {
+            file: "", // TODO!
+            approved: approved
+        }}}).then( (response) => {
+            console.log("Success approving audio!")
+            console.dir(response)
+            callback()
+        }).catch( (error) => {
+            console.log("error on approval")
+            console.error(error)
+        })
     }
     
     playOrPause() {
-        if(this.refs.PeaksObject.mainAudio.paused) {
+        if(this.refs.PeaksObject.refs.mainAudio.paused) {
             this.refs.PeaksObject.instance.player.play()
         } else {
             this.refs.PeaksObject.instance.player.pause()
@@ -131,13 +155,14 @@ class EditingPage extends React.Component {
     render() {
         console.log(this.state.whichSrc)
         // TODO: should be based on a query of some sort
+        // use public.check_moderator (SQL)
         const userIsModerator = true
         if (!userIsModerator) { return <NoEditPage /> }
-        
+        if (this.props.audio.loading) { return <LoadingPage /> }
         return(
             <div className='panel animated fadeIn' id='edit'>
                 <Translate  id='moderationHead' component="h1" content="edit.heading" />
-                <p><Translate content="edit.thisRecording" />{this.state.src[this.state.whichSrc]}</p> {/* The idea is that the moderator should be able to see what word was supposed to be being recorded, otherwise we could have silly errors! */}
+                <p><Translate content="edit.thisRecording" />{this.props.audio.file}</p> {/* The idea is that the moderator should be able to see what word was supposed to be being recorded, otherwise we could have silly errors! */}
                 <PeaksObject src={this.state.src[this.state.whichSrc]} ref="PeaksObject" />
                 <div id='topButtons' style={{display: "block"}}>
                     <div className="authbtn" onClick={this.playOrPause.bind(this)} ><Translate content="edit.playAll" /></div>
@@ -146,13 +171,79 @@ class EditingPage extends React.Component {
                     <div className="authbtn" onClick={this.zoom.bind(this, false)} ><Translate content="edit.zoomOut" /></div>
                 </div>
                 <div id='bottomButtons' style={{display: "block"}}>
-                <div className="authbtn" onClick={this.handleSubmit.bind(this, false)} ><Translate content="edit.acceptFull" /></div>
-                <div className="authbtn" onClick={this.handleSubmit.bind(this, true)} ><Translate content="edit.acceptSegment" /></div>
-                <div className="authbtn" onClick={this.handleReject.bind(this)} ><Translate content="edit.reject" /></div>
-                    </div>
+                    <div className="authbtn" onClick={this.handleSubmit.bind(this, false)} ><Translate content="edit.acceptFull" /></div>
+                    <div className="authbtn" onClick={this.handleSubmit.bind(this, true)} ><Translate content="edit.acceptSegment" /></div>
+                    <div className="authbtn" onClick={this.handleReject.bind(this)} ><Translate content="edit.reject" /></div>
+                </div>
             </div>
         )
     }
 }
 
-export default EditingPage
+class WrappedEditingPage extends React.Component {
+    constructor(props) {
+        super(props) // ...what does this do?
+        // set any requisite state here
+    }
+
+    render() {
+        // TODO: not certain about userId, router, and route here - all necessary?
+        return <EditingPage
+                    audio={this.props.moderationQuery}
+                    submitEdits={this.props.editAudioMutation}
+                    approveAudio={this.props.approveAudioMutation}
+                    userId={this.props.userId}
+                    router={this.props.router} route={this.props.route}
+                />
+    }
+}
+
+const moderationQuery = gql`
+query ($languageId: String!, $number: Int!){
+	getAudioSubmissions(languageId: $languageId, number: $number) {
+	    nodes {
+    	    file
+            speaker
+            item
+            stamp
+        }
+    }
+}`
+
+const moderationQueryConfig = {
+    name: 'moderationQuery',
+    options: (ownProps) => ({
+        variables: {
+            languageId: ownProps.username ? ownProps.native[0] : 'eng'
+        }
+    })
+}
+
+const approveAudioMutation = gql`
+mutation ($input: ModerateAudioInput!) {
+    moderateAudio(input: $input) {
+        clientMutationId
+    }
+}
+`
+
+const approveAudioMutationConfig = {
+    name: 'approveAudioMutation'
+}
+
+const editAudioMutation = gql`
+mutation ($input: EditAudioInput!) {
+    editAudio(input: $input) {
+        clientMutationId
+    }
+}`
+
+const editAudioMutationConfig = {
+    name: 'editAudioMutation'
+}
+
+export default compose(
+    graphql(moderationQuery, moderationQueryConfig),
+    graphql(approveAudioMutation, approveAudioMutationConfig),
+    graphql(editAudioMutation, editAudioMutationConfig))
+(withRouter(WrappedEditingPage))
