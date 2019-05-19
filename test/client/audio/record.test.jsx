d9825f40fc10e5706ba2a3be5cd19ca9917e7141
sack-art-velo <iamsackartvelo@protonmail.com>
2019-05-19 15:19:23 +0100

    Minor testing refactor; StartButton and StopButton fully tested
    I think that those buttons are fully tested, anyway. One more thing to
    consider would be testing the functionality of the functions they adopt
    in different states, but I reckon that’s more of an integration test.

--- test/client/audio/record.test.jsx
+++ test/client/audio/record.test.jsx
@@ -18,6 +18,7 @@ configure({ adapter: new Adapter() })
 // NOTE: there is a fair bit of code repetition in this file.
 // It may be a good idea to try to refactor that.
 // The below function is a partial refactor to reduce code repetition.
+// NOTE 2: The Mocha function `beforeEach` may be helpful in the refactoring effort.
 function assertFunctionRunsOnClick(dummyFunction, element, elementId, expectClicked, message) {
     chai.assert.equal(dummyFunction.called, false)
     element.find('#' + elementId).simulate('click')
@@ -25,66 +26,94 @@ function assertFunctionRunsOnClick(dummyFunction, element, elementId, expectClic
     chai.assert.equal(dummyFunction.called, expectClicked, message)
 }
 
-describe('StartButton', function() {
-    it('is enabled in done and wait mode', function() {
-        for (let mode of ["wait", "done"]) {
-            const dummyfunc = sinon.fake()
-            const start = mount(<StartButton mode={mode} next={0} max={10} callback={dummyfunc} />)
-            const message = "fails in " + mode + " mode"
-            assertFunctionRunsOnClick(dummyfunc, start, 'startButton', true, message)
-        }
-    })
-})
+const MODES = ["wait", "record", "done", "reRecordSingleToWait", "reRecordSingleToDone", "playback", "playbackAll"]
 
-describe('StopButton', function() {
-    it('is disabled in done and wait mode', function() {
-        for (let mode of ["wait", "done"]) {
-            const dummyfunc = sinon.fake()
-            const stop = mount(<StopButton mode={mode} next={0} max={10} callback={dummyfunc} />)
-            const message = "fails in " + mode + " mode"
-            assertFunctionRunsOnClick(dummyfunc, stop, 'stopButton', false, message)
-        }
-    })
-})
-
-describe('PlayAllButton', function() {
-    it('is enabled in done and wait mode', function() {
-        for (let mode of ["wait", "done"]) {
-            const dummyfunc = sinon.fake()
-            const playall = mount(<PlayAllButton mode={mode} recordedSoFar={2} playAllFunction={dummyfunc} />)
-            assertFunctionRunsOnClick(dummyfunc, playall, 'playAllButton', true)
-        }
-    })
-})
-
-// TODO TutorialButton function
-
-
-describe('ReRecord button', function() {
-    it('is disabled in all modes if this word has not been recorded yet, regardless of focus', function() {
-        for (let mode of ["wait", "record", "done", "reRecordSingleToWait", "reRecordSingleToDone", "playback", "playbackAll"]) {
-            for (let focused of [true, false]) {
+describe('Record page', function() {
+    describe('StartButton', function() {
+        const enabledModes = ["wait", "done", "record"]
+    
+        it('is enabled in done, wait, and record mode', function() {
+            for (let mode of enabledModes) {
                 const dummyfunc = sinon.fake()
-                const reRecord = mount(<ReRecord index={0} mode={mode} next={1} srcExists={false} callback={dummyfunc} focused={focused} />)
-                assertFunctionRunsOnClick(dummyfunc, reRecord, "firstReRecordWord", false)
+                const start = mount(<StartButton mode={mode} next={0} max={10} callback={dummyfunc} />)
+                const message = "fails in " + mode + " mode"
+                assertFunctionRunsOnClick(dummyfunc, start, 'startButton', true, message)
             }
-        }
+        })
+        it('is disabled in all other modes', function() {
+            for (let mode of MODES) {
+                if ( !(enabledModes.includes(mode)) ) {
+                    const dummyfunc = sinon.fake()
+                    const start = mount(<StartButton mode={mode} next={0} max={10} callback={dummyfunc} />)
+                    const message = "fails in " + mode + " mode"
+                    assertFunctionRunsOnClick(dummyfunc, start, 'startButton', false, message)
+                }
+            }
+        })
     })
-    it('is enabled in wait mode and done mode if this word has already been recorded, regardless of focus', function() {
-        for (let mode of ["wait", "done"]) {
-            for (let focused of [true, false]) {
+    
+    describe('StopButton', function() {
+        const enabledModes = ["record"]
+    
+        it('is enabled in record mode', function() {
+            for (let mode of enabledModes) {
                 const dummyfunc = sinon.fake()
-                const reRecord = mount(<ReRecord index={0} mode={mode} next={1} srcExists={true} callback={dummyfunc} focused={true} />)
-                assertFunctionRunsOnClick(dummyfunc, reRecord, "firstReRecordWord", true)
+                const stop = mount(<StopButton mode={mode} next={0} max={10} callback={dummyfunc} />)            
+                const message = "fails in " + mode + " mode"
+                assertFunctionRunsOnClick(dummyfunc, stop, 'stopButton', true, message)
             }
-        }
+        })
+        it('is disabled in all other modes', function() {
+            for (let mode of MODES) {
+                if ( !(enabledModes.includes(mode)) ) {
+                    const dummyfunc = sinon.fake()
+                    const stop = mount(<StopButton mode={mode} next={0} max={10} callback={dummyfunc} />)            
+                    const message = "fails in " + mode + " mode"
+                    assertFunctionRunsOnClick(dummyfunc, stop, 'stopButton', false, message)
+                }
+            }
+        })
     })
+    
+    describe('PlayAllButton', function() {
+        it('is enabled in done and wait mode', function() {
+            for (let mode of ["wait", "done"]) {
+                const dummyfunc = sinon.fake()
+                const playall = mount(<PlayAllButton mode={mode} recordedSoFar={2} playAllFunction={dummyfunc} />)
+                assertFunctionRunsOnClick(dummyfunc, playall, 'playAllButton', true)
+            }
+        })
+    })
+    
+    // TODO TutorialButton function
+    
+    
+    describe('ReRecord button', function() {
+        it('is disabled in all modes if this word has not been recorded yet, regardless of focus', function() {
+            for (let mode of MODES) {
+                for (let focused of [true, false]) {
+                    const dummyfunc = sinon.fake()
+                    const reRecord = mount(<ReRecord index={0} mode={mode} next={1} srcExists={false} callback={dummyfunc} focused={focused} />)
+                    assertFunctionRunsOnClick(dummyfunc, reRecord, "firstReRecordWord", false)
+                }
+            }
+        })
+        it('is enabled in wait mode and done mode if this word has already been recorded, regardless of focus', function() {
+            for (let mode of ["wait", "done"]) {
+                for (let focused of [true, false]) {
+                    const dummyfunc = sinon.fake()
+                    const reRecord = mount(<ReRecord index={0} mode={mode} next={1} srcExists={true} callback={dummyfunc} focused={true} />)
+                    assertFunctionRunsOnClick(dummyfunc, reRecord, "firstReRecordWord", true)
+                }
+            }
+        })
+    })
+    
+    // TODO PlaybackOne function
+    
+    // TODO WordRow function
+    
+    // TODO RecordPage object
+    
+    // TODO WrappedRecordPage object
 })
-
-// TODO PlaybackOne function
-
-// TODO WordRow function
-
-// TODO RecordPage object
-
-// TODO WrappedRecordPage object
