2d0f86bdf48bf9a69ddc0ef3856e4bb8c501f273
Guy Emerson <gete2@cam.ac.uk>
2019-07-01 15:07:48 +0100

    Upgrade mochatesting version

---
+++ test/client/minimal_failure.test.jsx
@@ -0,0 +1,10 @@
+describe('my object', function() {
+    this.slow(0.1)
+    it('test1', function() {
+        throw new Error('inside it')
+    })
+    this.slew(0.1)
+    it('test2', function() {
+        console.log('never gets here')
+    })
+})
