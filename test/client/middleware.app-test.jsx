0302c98ab03a40582008c5704f773ca8b03ce8c3
Guy Emerson <gete2@cam.ac.uk>
2019-08-09 15:07:21 +0100

    Add simple test for client adding JWT

--- test/client/middleware.app-test.jsx
+++ test/client/middleware.app-test.jsx
@@ -11,6 +11,14 @@ import { pingQuery } from '/lib/graphql'
 configure({ adapter: new Adapter() })
 
 describe('Apollo client', function() {
+    beforeEach(function(done) {
+        localStorage.removeItem('token')
+        client.clearStore().then(done)
+    })
+    afterEach(function() {
+        localStorage.removeItem('token')
+    })
+    
     it('allows React elements to make requests to the server', function(done) {
         // This element will call the server-side ping function
         // If the query succeeds, the test passes
@@ -24,11 +32,35 @@ describe('Apollo client', function() {
                 done()
                 return props.data.ping
             }
-        } 
+        }
         const PingTestElementWithData = graphql(pingQuery)(PingTestElement)
         const wrapper = mount(
             <ApolloProvider client={client}>
                 <PingTestElementWithData/>
+            </ApolloProvider>
+        )
+    })
+    
+    it('causes a server error if an invalid JWT is in localStorage', function(done) {
+        // Put a nonsense JWT in localStorage
+        localStorage.setItem('token', 'abc')
+        // This element will call the server-side ping function
+        // If the query fails, the test passes
+        const PingFailTestElement = function(props) {
+            if (props.data.loading) {
+                return 'loading'
+            } else if (props.data.error) {
+                console.dir(props.data.error)
+                done()
+                return 'error!' 
+            } else {
+                throw new Error('No server error for nonsense JWT!')
+            }
+        }
+        const PingFailTestElementWithData = graphql(pingQuery)(PingFailTestElement)
+        const wrapper = mount(
+            <ApolloProvider client={client}>
+                <PingFailTestElementWithData/>
             </ApolloProvider>
         )
     })
